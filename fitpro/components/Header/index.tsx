968d88b794c207a28aba731a608f661d8f850fc9
Bardasson <121575366+bardasson10@users.noreply.github.com>
2026-01-15 18:56:43 -0300

    #chore: removendo comentarios desnecessarios

--- fitpro/components/Header/index.tsx
+++ fitpro/components/Header/index.tsx
@@ -26,14 +26,13 @@ export const Header = () => {
   return (
     <header className="flex h-16 shrink-0 items-center justify-between border-b px-4 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 sticky top-0 z-10 w-full">
       <div className="flex items-center gap-2">
-        {/* Trigger da Sidebar sempre acessível */}
         <SidebarTrigger className="h-9 w-9 mb-0 sm:mb-4" />
         
         <div className="flex flex-col justify-center overflow-hidden">
           <h1 className="text-sm font-semibold leading-none tracking-tight sm:text-lg truncate">
             {getPageTitle()}
           </h1>
-          {/* Escondemos a descrição em telas muito pequenas se necessário, ou diminuímos */}
+
           <p className="text-[11px] mt-1 text-muted-foreground sm:text-xs truncate">
             {getDescription()}
           </p>
